2780661159894192967de8c16b23bbcf6b4c256b
Supriyo Shankar Ghosh <89015232+supriyo72@users.noreply.github.com>
2023-01-06 22:00:41 +0530

    Add files via upload

---
+++ Arrays.java
@@ -0,0 +1,31 @@
+public class Arrays {
+    public static void main(String[] args) {
+        // 1
+        // int [] marks= new int[5];
+
+
+        // 2
+        // int [] marks;
+        // marks= new int[6];
+
+
+        // 3
+        // int [] marks= new int[3];
+        // marks[0]= 46;
+        // marks[1]= 4;
+        // marks[2]= 65;
+
+        // System.out.println(marks[2]);
+
+
+        // 4
+        int [] marks= {24,58,76,34,98,32};
+        float [] mark= {24.87f,58.986f,76.786f,34.56f,98.9f,32.21f};
+        String [] studs= {"aman","azad","narang","raj"};
+        System.out.println(marks.length);
+        System.out.println(marks[5]);
+        System.out.println(mark[4]);
+        System.out.println(studs[2]);
+
+    }
+}
