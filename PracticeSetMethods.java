0c4ad42889c594806401ec358ac533cfa938ca42
Supriyo Shankar Ghosh <89015232+supriyo72@users.noreply.github.com>
2023-01-22 18:42:14 +0530

    Update PracticeSetMethods.java

--- PracticeSetMethods.java
+++ PracticeSetMethods.java
@@ -122,8 +122,10 @@ sum(3)= 3+2+sum(1)=3+2+1=6
 // q-5 nth term of fibonacci series using recursion,nth means it starts from 1
 // 0, 1, 1, 2, 3, 5, 8, 13, 21, 34,...
 /*
-fib(6)=fib(5)+fib(4)
-      fib(4)+fib(3)+fib(3)+fib(2)
+fib(6)=fib(5)+fib(4)+
+      fib(4)+fib(3)+
+      fib(3)+fib(2)+
+      fib(2)+fib(1)
 */
 
 // public class PracticeSetMethods {
